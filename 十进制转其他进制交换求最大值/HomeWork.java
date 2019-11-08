2f1b4fb325ff3b0236e9f972837239d70824b3a9
WallisEdward <2427058190@qq.com>
2019-11-08 11:32:24 +0800

    提交

---
+++ 十进制转其他进制交换求最大值/HomeWork.java
@@ -0,0 +1,50 @@
+public class HomeWork{
+    public static void main(String[] args) {
+        // 给定一个十进制整数, 如何转成二进制形式? 如何转成十六进制形式?
+        int a = 10;
+        System.out.println(Integer.toBinaryString(a));// 转二进制
+        System.out.println(Integer.toOctalString(a));// 转八进制
+        System.out.println(Integer.toHexString(a));// 转十六进制 
+        // 2. 写代码实现: 给定三个 int 变量, 求其中的最大值和最小值.
+        // int a = 1; 
+        // int b = 2; 
+        // int c = 3; 
+        // if (a > b) {
+        //     if(a > c){
+        //         System.out.println("最大值是a="+a);
+        //         if (b > c) {
+        //             System.out.println("最小值是c="+c);
+        //         } else {
+        //             System.out.println("最小值是b="+b);
+        //         }
+        //     }
+        //     else {
+        //         System.out.println("最大值是c="+c);
+        //         System.out.println("最小值是b="+b);
+        //     }
+        // } else {
+        //     if(b > c){
+        //         System.out.println("最大值是b="+b);
+        //         if (a > c) {
+        //             System.out.println("最小值是c="+c);
+        //         } else {
+        //             System.out.println("最小值是a="+a);
+        //         }
+        //     }
+        //     else {
+        //         System.out.println("最大值是c="+c);
+        //         System.out.println("最小值是a="+a);
+        //     }
+        // }
+        // 1. 写代码实现: 给定两个 int 变量, 交换变量的值.
+        // int a = 1;
+        // int b = 2;
+        // int c;
+        // c = a;
+        // a = b;
+        // b = c;
+        // System.out.println(a);
+        // System.out.println(b);
+    }
+}
+
