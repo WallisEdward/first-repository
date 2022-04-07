59926df7a1c2456d5689cbfad1424fedb6737150
WallisEdward <2427058190@qq.com>
2022-04-07 21:02:05 +0800

    递归

---
+++ 二叉树的最大深度/TreeNode.java
@@ -0,0 +1,12 @@
+public class TreeNode {
+    Integer val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(Integer val) { this.val = val; }
+    TreeNode(Integer val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+     }
+}
